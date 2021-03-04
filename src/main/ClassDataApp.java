659941a0f55014798279a4313b1a7725c27a7f94
RyanFin <ryanfinlayson125@hotmail.com>
2021-03-04 23:27:15 +0000

    Add ClassDataArray Person java program

--- src/main/ClassDataApp.java
+++ src/main/ClassDataApp.java
@@ -3,7 +3,48 @@ package main;
 public class ClassDataApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//	define array size
+		int maxSize = 100;
+//		array obj
+		ClassDataArray arr;
+//		instantiate array object
+		arr = new ClassDataArray(maxSize);
+		
+//		insert the Person objects into the Object Array
+		arr.insert("Evans", "Patty", 24);
+		arr.insert("Super", "Mario", 45);
+		arr.insert("Yee", "Tom", 15);
+		arr.insert("Adams", "Smith", 34);
+		arr.insert("Yakamoto", "Satoshi", 25);
+		arr.insert("Vang", "Minh", 24);
+		
+		
+//		display items
+		arr.displayA();
+		
+//		search for an item
+		Person found;
+		String searchKey = "Mario";
+		
+		found = arr.find(searchKey);
+		
+		if (found != null) {
+			System.out.println("Found ");
+			found.displayPerson();
+		} else {
+			System.out.println("Person " + searchKey + "not found in arr....");
+		}
+		
+		System.out.println("Deleting Mario, Smith, and Minh");
+		arr.delete("Mario");
+		arr.delete("Smith");
+		arr.delete("Minh");
+		
+		
+		arr.displayA();
+		
+		
+		
 
 	}
 
--- src/main/ClassDataArray.java
+++ src/main/ClassDataArray.java
@@ -1,5 +1,86 @@
 package main;
 
+// Algorithms and Data structures page 67
 public class ClassDataArray {
+	
+//	reference to the array
+	private Person[] a;
+	
+//	number of data items
+	private int nElems;
+	
+//	constructor
+	public ClassDataArray(int max){
+//		create the array
+		a = new Person[max];
+//		no items yet
+		nElems = 0;
+	}
+	
+//	find specified element
+	public Person find(String searchName) {
+		int j;
+//		for each element in array of Person objects  
+		for(j = 0; j < nElems; j++) {
+//			found item?
+			if(a[j].getLastName().equals(searchName)) {
+//				exit loop before end
+				break;
+			}
+		}
+		
+//		gone to end?
+		if(j ==  nElems) {
+//			yes, then you cannot find the target element
+			return null;
+		} else {
+//			otherwise you found it
+			return a[j];
+		}	
+	}
+	
+	public void insert(String last, String first, int age) {
+//		add new Person to the array
+		a[nElems] = new Person(last, first, age);
+//		increase the counter value
+		nElems++;
+	}
+	
+//	delete the specified element with a search by name
+	public boolean delete(String searchName){
+		int j;
+		
+		for (j = 0; j < nElems; j++) {
+			if(a[j].getLastName().equals(searchName)) {
+//				exit current iteration at the found item
+				break;
+			}
+		}
+		
+		if(j == nElems){
+//			 Can't find the target elem
+			return false;
+		} else {
+			for (int k = 0; k < nElems; k++) {
+//				shift down
+				a[k] = a[k + 1];
+//				decrement size
+				nElems --;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+//	display all array elems 
+	public void displayA() {
+		for (int i = 0; i < nElems; i++) {
+			a[i].displayPerson();
+		}
+	}
+	
+	
+	
+	
 
 }
